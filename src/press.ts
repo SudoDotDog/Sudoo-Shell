df5f551147905ba7aafb386c1fc163d8a9b587f8
WMXPY <wm@mengw.io>
2020-08-11 15:33:46 -0500

    init press command

---
+++ src/press.ts
@@ -0,0 +1,27 @@
+/**
+ * @author WMXPY
+ * @namespace Shell
+ * @description Press
+ */
+
+const escCharCode: number = 27;
+
+export const waitForKeyPress = (): Promise<boolean> => {
+
+    process.stdin.setRawMode(true);
+
+    return new Promise<boolean>((resolve: (result: boolean) => void) => {
+
+        process.stdin.on('data', (buffer: Buffer) => {
+
+            const charCode: number = buffer.toString().charCodeAt(0);
+            process.stdin.setRawMode(false);
+
+            if (charCode === escCharCode) {
+                resolve(false);
+            } else {
+                resolve(true);
+            }
+        });
+    });
+};
