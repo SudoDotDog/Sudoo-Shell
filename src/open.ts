da985cdfa77a22d5373b64ffc4a3cb004aa50739
WMXPY <wm@mengw.io>
2020-08-11 15:32:59 -0500

    add shell open command

---
+++ src/open.ts
@@ -0,0 +1,34 @@
+/**
+ * @author WMXPY
+ * @namespace Shell
+ * @description Open
+ */
+
+import { exec } from "child_process";
+
+const getOpenCommand = (): string => {
+
+    switch (process.platform) {
+        case 'win32':
+            return 'start';
+    }
+
+    return 'open';
+};
+
+export const openFile = (file: string): Promise<void> => {
+
+    return new Promise<void>((resolve: () => void, reject: (reason: any) => void) => {
+
+        const openCommand: string = getOpenCommand();
+        exec(`${openCommand} ${file}`, (error: any, stdout: string, stderr: string) => {
+
+            if (!error) {
+                resolve();
+                return;
+            }
+
+            reject(stderr);
+        });
+    });
+};
