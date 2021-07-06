f8a56abf2168facaa3bafa8f01586c1686277dbb
Meng Wang <wm@mengw.io>
2021-07-06 21:45:21 +0000

    fixl itn

--- src/execution.ts
+++ src/execution.ts
@@ -7,7 +7,7 @@
 import { exec } from "child_process";
 
 export const shellExecuteCommand = (command: string): Promise<string> => {
-    
+
     return new Promise<string>((resolve: (out: string) => void, reject: (reason: any) => void) => {
 
         exec(command, (error: any, stdout: string, stderr: string) => {
